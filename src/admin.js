73f1e5aad629a405d254759940cc8e6caf8bd851
侯磊 <lhou@cheweilai.cn>
2019-03-19 16:36:34 +0800

    页面骨架

--- src/admin.js
+++ src/admin.js
@@ -3,6 +3,7 @@ import {Row, Col} from "antd";
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
+import Home from './pages/home'
 import './style/common.less'
 
 export default class Admin extends React.Component {
@@ -17,7 +18,7 @@ export default class Admin extends React.Component {
 
                     </Header>
                     <Row className='content'>
-                        content
+                        <Home></Home>
                     </Row>
                     <Footer/>
                 </Col>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './index.less'
 
 export default class Footer extends React.Component {
     render() {
-        return <div>
-            footer
+        return <div className='footer'>
+            版权所有：EVOLUTION（推荐使用谷歌浏览器，可以获得更佳操作页面体验） 技术支持：EVOLUTION
         </div>
     }
 }
