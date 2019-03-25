f1ca26eea2852cff8bdfd8c1d210eb6eaeab5357
侯磊 <lhou@cheweilai.cn>
2019-03-25 16:41:48 +0800

    一些按钮

--- src/pages/ui/buttons/index.js
+++ src/pages/ui/buttons/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card, Button } from 'antd'
+import './ui.less'
 
 export default class Buttons extends React.Component {
   render() {
@@ -7,7 +8,27 @@ export default class Buttons extends React.Component {
     return (
       <div>
         <Card title="基础按钮">
-          <Button type="primary">EVO</Button>
+          <Button type="primary">主要按钮</Button>
+          <Button type="">普通按钮</Button>
+          <Button type="dashed">虚线按钮</Button>
+          <Button type="danger">警告</Button>
+          <Button disabled>禁用按钮</Button>
+        </Card>
+        <Card title="图形按钮">
+          <Button icon="plus">创建</Button>
+          <Button icon="edit">编辑</Button>
+          <Button icon="delete">删除</Button>
+          <Button icon="search" shape='circle'></Button>
+          <Button type='primary' icon='search'>搜素</Button>
+          <Button type='primary' icon='download'>下载</Button>
+        </Card>
+        <Card title="载入按钮">
+          {/* <Button icon="plus">创建</Button>
+          <Button icon="edit">编辑</Button>
+          <Button icon="delete">删除</Button>
+          <Button icon="search" shape='circle'></Button>
+          <Button type='primary' icon='search'>搜素</Button>
+          <Button type='primary' icon='download'>下载</Button> */}
         </Card>
       </div>
     )
